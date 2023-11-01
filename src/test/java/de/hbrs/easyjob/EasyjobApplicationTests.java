890f1fb37cfe989f5dca0113f2eb42711b1d12a1
Emre Cetin <emre.cetin@smail.inf.h-brs.de>
2023-11-01 20:54:48 +0100

    Vaadin 23 init

---
+++ src/test/java/de/hbrs/easyjob/EasyjobApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hbrs.easyjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyjobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
