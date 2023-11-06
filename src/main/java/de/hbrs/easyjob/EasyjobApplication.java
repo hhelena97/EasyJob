dcaafa0c64cc829597070cf3a62515304e413031
Emre Cetin <emre.cetin@smail.inf.h-brs.de>
2023-11-06 19:16:53 +0100

    Repository-API fix & Development Configuration hinzugefügt

--- src/main/java/de/hbrs/easyjob/EasyjobApplication.java
+++ src/main/java/de/hbrs/easyjob/EasyjobApplication.java
@@ -1,10 +1,16 @@
 package de.hbrs.easyjob;
 
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EasyjobApplication {
+@PWA(
+		name = "EasyJob - Easy gesucht, easy gefunden",
+		shortName = "EasyJob"
+)
+public class EasyjobApplication implements AppShellConfigurator {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EasyjobApplication.class, args);
