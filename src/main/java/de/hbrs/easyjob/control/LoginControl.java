be60ac156f31326b1bc5e48ca09d0afc33129b73
Sarah <mail@sarahlukas.de>
2023-11-16 17:55:15 +0100

    Es gibt noch keine Routen zur Weiterleitung, funktioniert aber grundsaetzlich.

--- src/main/java/de/hbrs/easyjob/control/LoginControl.java
+++ src/main/java/de/hbrs/easyjob/control/LoginControl.java
@@ -28,12 +28,24 @@ public class LoginControl {
         this.repository = personRepository;
     }
 
-    // wenn authenticate ein false zurückgibt, soll im View ein Popup "Bitte eingegebene Daten überprüfen" (oder so ähnlich)
-    // true führt direkt zur nächsten Seite
+    /**
+     * Authentifiziert einen Benutzer über E-Mail und Passwort.
+     * Die Methode überprüft die Datenbankverbindung, sucht nach der Person mit der angegebenen E-Mail
+     * und vergleicht dann das eingegebene Passwort mit dem in der Datenbank gespeicherten Passwort.
+     *
+     * @param email    Die E-Mail des Benutzers und damit auch sein/ihr Anmeldename.
+     * @param password Das Passwort des Benutzers.
+     * @return false, wenn die Authentifizierung fehlschlägt (das soll dann im View ein Popup auslösen)
+     * falls die Person authentifiziert werden kann, wird der Nutzer zur nächsten Seite weitergeleitet
+     * (es wird ein True zurückgegeben, dass aber (hoffentlich) irrelevant ist).
+     * Damit man auch ohne Weiterleitung sieht, was die Methode gerade macht, gibt es noch die vielen Ausgaben in der
+     * Konsole. Die lösche ich später, wieder raus.
+     */
     public boolean authenticate(String email, String password) {
         Person person;
         try {
             person = repository.findByEmail(email);
+            //Verknüpft die Person mit der Person der Datenbank, die zu dieser E-Mail-Adresse gespeichert ist
             System.out.println("Datenbankverbindung erfolgreich.");
         } catch ( org.springframework.dao.DataAccessResourceFailureException e ) {
             System.out.println("Problem mit der Datenbankverbindung.");
@@ -48,13 +60,18 @@ public class LoginControl {
             return false;
         }
         String eingabePW = password;
+        //Wenn das Passwort in der Registrierung gehasht wird,
+        //TODO: das eingegebene Passwort ebenfalls hashen
         String dbPW = person.getPasswort();
 
         if (eingabePW.equals(dbPW)){
+            //prüft ob das Passwort zum gespeicherten Passwort passt
             System.out.println("Passwort stimmt.");
             this.personDTO = repository.findPersonByEmail(email);
+            //erstellt ein DTO aus den Daten der Datenbank, die zu der E-Mail-Adresse gehören
 
             if (person instanceof Student){
+                //wenn es ein Student ist, belege das stuendtDTO
                 this.studentDTO = (StudentDTOimpl) personDTO;
                 System.out.println("Es ist ein Student.");
                 //TODO: weiter zur Studenten-Startseite
@@ -62,19 +79,24 @@ public class LoginControl {
             }
 
             if (person instanceof Unternehmensperson){
+                //wenn es eine Unternehmensperson ist, belege das UnternehmenspersonDTO
                 this.unternpersonDTO = (UnternehmenspersonDTOimpl) personDTO;
                 System.out.println("Es ist eine Unternehmensperson.");
                 //TODO: weiter zur Unternehmer-Startseite
                 return true;
             }
             //es ist eine Person, aber kein Student oder Unternehmensperson
+            //hier kommt später die Weiterleitung zur Admin-Seite (Sprint 2)
             return true;
         }
         //Sonstiges Problem
-        //bleibt auf der Startseite
+        //deshalb kein weiteres Routing, sondern wir bleiben auf der Startseite
         return false;
     }
 
+    /**
+    Methode aus der Carlook-Vorlage
+     */
     public PersonDTOimpl getCurrentPerson(){
         return this.personDTO;
 
