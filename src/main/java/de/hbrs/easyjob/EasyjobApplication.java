d1ee46a91caedbe214319360cd009fd2d091abc6
Emre Cetin <emre.cetin@smail.inf.h-brs.de>
2023-11-26 12:26:33 +0100

    fixed deployment error

--- src/main/java/de/hbrs/easyjob/EasyjobApplication.java
+++ src/main/java/de/hbrs/easyjob/EasyjobApplication.java
@@ -4,13 +4,14 @@ import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.PWA;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @PWA(
 		name = "EasyJob - Easy gesucht, easy gefunden",
 		shortName = "EasyJob"
 )
-public class EasyjobApplication implements AppShellConfigurator {
+public class EasyjobApplication extends SpringBootServletInitializer implements AppShellConfigurator {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EasyjobApplication.class, args);
