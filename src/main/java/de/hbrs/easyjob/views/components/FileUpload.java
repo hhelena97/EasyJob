11f6f9828bc8d8a1948fd88114e3e1ca811c728e
hheyen2s <helena.heyen@smail.inf.h-brs.de>
2023-12-13 16:05:46 +0100

    Student Profilbild Upload

---
+++ src/main/java/de/hbrs/easyjob/views/components/FileUpload.java
@@ -0,0 +1,83 @@
+package de.hbrs.easyjob.views.components;
+
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.server.StreamResource;
+import org.apache.commons.io.IOUtils;
+
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.stream.ImageInputStream;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+
+/**
+ * FileUpload Hilfsklasse für den Upload von verschiedenen Dateitypen.
+ * Für weitere Bearbeitung siehe Processor-Klassen.
+ */
+
+public class FileUpload {
+
+    /**
+     * imageUpload konvertiert eine hochgeladene Bilddatei zu einer Vaadin Image Komponente
+     * @param fileData Imputstream von der Upload Komponente
+     * @param fileName String Dateiname
+     * @return Bild in Originalgröße
+     */
+    public static Image imageUpload(InputStream fileData, String fileName) {
+        Image image = new Image();
+        try {
+            byte[] bytes = IOUtils.toByteArray(fileData);
+            image.getElement().setAttribute("src", new StreamResource(
+                    fileName, () -> new ByteArrayInputStream(bytes)));
+            try (ImageInputStream in = ImageIO.createImageInputStream(
+                    new ByteArrayInputStream(bytes))) {
+                final Iterator<ImageReader> readers = ImageIO
+                        .getImageReaders(in);
+                if (readers.hasNext()) {
+                    ImageReader reader = readers.next();
+                    try {
+                        //Set Originalgröße
+                        reader.setInput(in);
+                        image.setWidth(reader.getWidth(0) + "px");
+                        image.setHeight(reader.getHeight(0) + "px");
+                    } finally {
+                        reader.dispose();
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();}
+        return image;
+    }
+
+    /**
+     * squareImageUpload konvertiert eine hochgeladene Bilddatei im Querformat zu einer quadratischen Image-Komponente
+     * @param fileData Imputstream von der Upload-Komponente
+     * @param fileName String Dateiname
+     * @param targetSize Wunschgröße in px
+     * @return quadratisches Bild
+     */
+    public static Image squareImageUpload(InputStream fileData, String fileName, int targetSize) {
+        Image image = new Image();
+        BufferedImage bufferedImage;
+        boolean png = false;
+        try {
+            if (fileName.contains("png")){
+                bufferedImage = ImageIO.read(fileData);
+                png = true;
+            }else {
+                byte[] bytes = IOUtils.toByteArray(fileData);
+                ByteArrayInputStream inputStream = new ByteArrayInputStream(bytes);
+                bufferedImage = ImageIO.read(inputStream);
+            }
+            //Weiter zu Image Processor für crop
+            image = ImageProcessor.cropToSquare(bufferedImage, fileName, targetSize, png);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return image;
+    }
+}
--- src/main/java/de/hbrs/easyjob/views/student/registrieren/Schritt3View.java
+++ src/main/java/de/hbrs/easyjob/views/student/registrieren/Schritt3View.java
@@ -6,9 +6,15 @@ import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.upload.Upload;
+import com.vaadin.flow.component.upload.receivers.MemoryBuffer;
 import com.vaadin.flow.router.PageTitle;
 import de.hbrs.easyjob.entities.Student;
+import de.hbrs.easyjob.views.components.FileUpload;
 import de.hbrs.easyjob.views.templates.RegistrierenSchritt;
+
+import java.io.InputStream;
 
 @PageTitle("Fast geschafft...")
 public class Schritt3View extends RegistrierenSchritt {
@@ -23,48 +29,66 @@ public class Schritt3View extends RegistrierenSchritt {
     public void insertContent() {
         setAlignItems(Alignment.CENTER);
 
+        //Bildrahmen
         Div rahmen = new Div();
         rahmen.addClassName("profile-picture-frame");
-
         Image ellipse = new Image("images/Ellipse-Lila-Groß.png", "Bildumrandung");
         ellipse.addClassName("profile-picture-background");
         rahmen.add(ellipse);
 
-        profilBild = new Image("images/blank-profile-picture.png", "Profilbild Platzhalter");
-        Div bildDiv = new Div(profilBild);
-        profilBild.addClassName("picture-round");
+        //Platzhalter Bild
+        Image platzhalterBild = new Image("images/blank-profile-picture.png", "Profilbild Platzhalter");
+        profilBild = platzhalterBild;
+        Div bildDiv = new Div(platzhalterBild);
+        platzhalterBild.addClassName("picture-round");
         rahmen.add(bildDiv);
 
+        //Bild bearbeiten Button
         Button bildBearbeiten = new Button("Bild bearbeiten(optional)", new Icon(VaadinIcon.PENCIL));
         bildBearbeiten.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
         bildBearbeiten.addClassName("cancel");
 
-        /*
-        Upload upload = new Upload(new FileBuffer());
+        //Setup Upload
+        MemoryBuffer buffer = new MemoryBuffer();
+        Upload upload = new Upload(buffer);
+        upload.setDropAllowed(false);
+        upload.setAcceptedFileTypes("image/jpeg", "image/jpg, image/png");
+        upload.setUploadButton(bildBearbeiten);
+        Div uploadListe = new Div();
 
-        upload.setMaxFiles(1);
-        upload.setAcceptedFileTypes("image/*");
-        Image uploadedImage = new Image();
-
-
-        bildBearbeiten.addClickListener(e -> triggerUpload(upload));
-
+        //Profilbild Upload
         upload.addSucceededListener(event -> {
-            InputStream inputStream = ((FileBuffer) upload.getReceiver()).getInputStream();
-            try {
-                uploadedImage.getElement().executeJs("this.src = URL.createObjectURL($0);", new Object[]{inputStream});
-            } finally {
-                try {
-                    inputStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+            InputStream fileData = buffer.getInputStream();
+            String fileName = event.getFileName();
+
+            //Custom Upload Liste
+            Div fileInfo = new Div();
+            fileInfo.setText(fileName);
+            fileInfo.addClassName("file-info");
+            Button remove = new Button(new Icon(VaadinIcon.CLOSE));
+            remove.addClassName("remove-file");
+            remove.addThemeVariants(ButtonVariant.LUMO_SMALL);
+            HorizontalLayout dateiName = new HorizontalLayout(fileInfo, remove);
+            dateiName.setAlignItems(Alignment.CENTER);
+
+            //Profilbild entsprechend wechseln
+            Image neuesBild = FileUpload.squareImageUpload(fileData, fileName, 242);
+            upload.addAllFinishedListener(e ->{
+                profilBild = neuesBild;
+                uploadListe.add(dateiName);
+                neuesBild.addClassName("picture-round");
+                bildDiv.replace(platzhalterBild, neuesBild);});
+            bildBearbeiten.addClassName("cancel-disabled");
+            remove.addClickListener(e -> {
+                profilBild = platzhalterBild;
+                upload.clearFileList();
+                bildDiv.replace(neuesBild, platzhalterBild);
+                dateiName.removeAll();
+                bildBearbeiten.addClassName("cancel");
+            });
         });
 
-         */
-
-        add(rahmen, bildBearbeiten);
+        add(rahmen, upload, uploadListe);
     }
 
     public boolean checkRequirementsAndSave() {
