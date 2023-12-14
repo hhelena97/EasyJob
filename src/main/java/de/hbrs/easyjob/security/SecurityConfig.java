2d5339f935f3f42e66a6201cc34cd6a4e5d8dff4
Emre Cetin <emre.cetin@smail.inf.h-brs.de>
2023-12-14 14:23:59 +0100

    Umstellung auf Argon2

--- src/main/java/de/hbrs/easyjob/security/SecurityConfig.java
+++ src/main/java/de/hbrs/easyjob/security/SecurityConfig.java
@@ -1,20 +1,16 @@
 package de.hbrs.easyjob.security;
 
-import com.vaadin.flow.spring.security.VaadinWebSecurityConfigurerAdapter;
 import de.hbrs.easyjob.services.MyUserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.TaskExecutor;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.task.DelegatingSecurityContextAsyncTaskExecutor;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
@@ -55,7 +51,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService)
-                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+                .passwordEncoder(getPasswordEncoder());
+    }
+
+    public static Argon2PasswordEncoder getPasswordEncoder() {
+        return new Argon2PasswordEncoder(16, 32, 2, 60000, 10);
     }
 
 }
