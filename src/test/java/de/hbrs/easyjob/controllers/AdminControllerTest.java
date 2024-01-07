1aa90c970caf535d9cf1b96f185041fbcb86e81d
Vanessa <vanessa.jagau@smail.inf.h-brs.de>
2024-01-07 09:51:06 +0100

    weitere Tests geschrieben

--- src/test/java/de/hbrs/easyjob/controllers/AdminControllerTest.java
+++ src/test/java/de/hbrs/easyjob/controllers/AdminControllerTest.java
@@ -1,6 +1,7 @@
 package de.hbrs.easyjob.controllers;
 
 import de.hbrs.easyjob.entities.Admin;
+import de.hbrs.easyjob.entities.Student;
 import de.hbrs.easyjob.repositories.PersonRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,6 +9,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.transaction.Transactional;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -37,6 +41,7 @@ class AdminControllerTest {
 
     @Test
     @DisplayName("Testet, ob ein valider Admin angelegt werden kann")
+    @Transactional
     void createValidAdminTest() {
         /* ********************** ARRANGE ********************** */
         admin.setPasswort("Test123!");
@@ -54,6 +59,7 @@ class AdminControllerTest {
 
     @Test
     @DisplayName("Testet, ob ein nicht valider Admin angelegt werden kann")
+    @Transactional
     void createNichtValidAdminTest() {
         /* ********************** ARRANGE ********************** */
         admin.setEmail("nichtvalideradmin.de");
@@ -67,6 +73,7 @@ class AdminControllerTest {
 
     @Test
     @DisplayName("Testet die Deaktivierung eines Admins")
+    @Transactional
     void deleteAdminTest() {
         /* ********************** ARRANGE ********************** */
         admin.setPasswort("Test123!");
@@ -86,6 +93,7 @@ class AdminControllerTest {
 
     @Test
     @DisplayName("Testet die Deaktivierung für nicht-existierenden Admin")
+    @Transactional
     void deleteNotExistingAdminTest() {
         /* ********************** ARRANGE ********************** */
         admin.setPasswort("Test123!");
@@ -106,6 +114,7 @@ class AdminControllerTest {
 
     @Test
     @DisplayName("Testet, ob neues Passwort angelegt wurde für einen existierenden Admin")
+    @Transactional
     void newPasswordExistingAdminTest() {
         /* ********************** ARRANGE ********************** */
         admin.setPasswort("Test123!");
@@ -127,6 +136,7 @@ class AdminControllerTest {
 
     @Test
     @DisplayName("Testet, ob neues Passwort angelegt wurde für einen nicht existierenden Admin")
+    @Transactional
     void newPasswordNotExistingAdminTest() {
         /* ********************** ARRANGE ********************** */
         admin.setPasswort("Test123!");
@@ -147,6 +157,7 @@ class AdminControllerTest {
 
     @Test
     @DisplayName("Testet, ob neues nicht valides Passwort angelegt wurde für einen existierenden Admin")
+    @Transactional
     void newInvalidPasswordExistingAdminTest() {
         /* ********************** ARRANGE ********************** */
         admin.setPasswort("Test123!");
@@ -164,5 +175,106 @@ class AdminControllerTest {
         /* *********************** ASSERT ********************** */
         assertEquals(expected, admin.getPasswort());
         assertFalse(actual);  // false, weil nichts geändert wurde, da Passwort invalid
+    }
+
+    @Test
+    @DisplayName("Testet changePassword() von einer existierenden Person")
+    @Transactional
+    void changePasswordOfExistingPersonTest() {
+        /* ********************** ARRANGE ********************** */
+        Student p1 = new Student();
+        p1.setVorname("Max");
+        p1.setNachname("Mustermann");
+        p1.setEmail("max.mustermann@example.de");
+        p1.setPasswort("ichbimsDerGr0sst1!!!");
+        personRepository.save(p1);
+        String old = personRepository.findByEmail("max.mustermann@example.de").getPasswort();
+
+        /* ************************ ACT ************************ */
+        boolean actual = adminController.changePassword("max.mustermann@example.de", "ilikebigbiblesandIcannotli3!");
+        String neu = personRepository.getReferenceById(p1.getId_Person()).getPasswort();
+
+        /* ********************** TEAR DOWN ******************** */
+        personRepository.delete(personRepository.getReferenceById(p1.getId_Person()));
+
+        /* *********************** ASSERT ********************** */
+        assertNotEquals(old, neu);
+        assertTrue(actual);
+
+    }
+
+    @Test
+    @DisplayName("Testet changePassword() von einer deaktivierten Person")
+    @Transactional
+    void changePasswordOfDeactivatedPersonTest() {
+        /* ********************** ARRANGE ********************** */
+        Student p1 = new Student();
+        p1.setVorname("Max");
+        p1.setNachname("Mustermann");
+        p1.setEmail("max.mustermann@example.de");
+        p1.setPasswort("ichbimsDerGr0sst1!!!");
+        p1.setAktiv(false);
+        personRepository.save(p1);
+        String old = personRepository.findByEmail("max.mustermann@example.de").getPasswort();
+
+        /* ************************ ACT ************************ */
+        boolean actual = adminController.changePassword("max.mustermann@example.de", "ilikebigbiblesandIcannotli3!");
+        String neu = personRepository.getReferenceById(p1.getId_Person()).getPasswort();
+
+        /* ********************** TEAR DOWN ******************** */
+        personRepository.delete(personRepository.getReferenceById(p1.getId_Person()));
+
+        /* *********************** ASSERT ********************** */
+        assertFalse(actual);
+        assertNotEquals(old,neu);
+    }
+
+    @Test
+    @DisplayName("Testet changePassword() von einer nicht existierenden Person")
+    @Transactional
+    void changePasswordOfNotExistingPersonTest() {
+        /* ********************** ARRANGE ********************** */
+        Student p1 = new Student();
+        p1.setVorname("Max");
+        p1.setNachname("Mustermann");
+        p1.setEmail("max.mustermann@example.de");
+        p1.setPasswort("ichbimsDerGr0sst1!!!");
+
+        /* ************************ ACT ************************ */
+        boolean actual = adminController.changePassword("max.mustermann@example.de", "ilikebigbiblesandIcannotli3!");
+
+        /* *********************** ASSERT ********************** */
+        assertFalse(actual);
+    }
+
+    @Test
+    @DisplayName("Testet getAllAdmins()")
+    @Transactional
+    void getAllAdminsTest() {
+        /* ********************** ARRANGE ********************** */
+        admin.setPasswort("Test123!");
+        admin.setEmail("tolleradmin@yeah.de");
+        adminController.createAdmin(admin);
+        List<Admin> expected = List.of(admin);
+
+        /* ************************ ACT ************************ */
+        List<Admin> actual = adminController.getAllAdmins();
+
+        // 2. Admin hinzufügen und auch noch finden
+        Admin admin2 = new Admin();
+        admin2.setPasswort("Test123!");
+        admin2.setEmail("vieltollereradmin@boo-yeah.de");
+        adminController.createAdmin(admin2);
+        List<Admin> expected2 = List.of(admin, admin2);
+
+        List<Admin> actual2 = adminController.getAllAdmins();
+
+        /* ********************** TEAR DOWN ******************** */
+        personRepository.delete(personRepository.getReferenceById(admin.getId_Person()));
+        personRepository.delete(personRepository.getReferenceById(admin2.getId_Person()));
+
+        /* *********************** ASSERT ********************** */
+        assertEquals(expected, actual);
+        assertEquals(expected2, actual2);
     }
 }
