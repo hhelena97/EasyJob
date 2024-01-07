41d3f94a6efa45f5f86de368982f474ec4875ec5
Sarah <mail@sarahlukas.de>
2024-01-07 13:57:08 +0100

    Noch Fehler im ProfilMelden

---
+++ src/main/java/de/hbrs/easyjob/views/admin/MeldungDetailProfileView.java
@@ -0,0 +1,11 @@
+package de.hbrs.easyjob.views.admin;
+
+public class MeldungDetailProfileView extends MeldungDetailView{
+
+    String profil;
+
+    public MeldungDetailProfileView(String profil){
+        super(profil);
+
+    }
+}
