1702fdddedf02b94c721199cb336600a6b2d5f39
Sarah <mail@sarahlukas.de>
2024-01-15 11:26:18 +0100

    Admin in Testing (und 404 ist wieder aktiviert)

--- src/main/java/de/hbrs/easyjob/views/allgemein/CustomNotFoundView.java
+++ src/main/java/de/hbrs/easyjob/views/allgemein/CustomNotFoundView.java
@@ -5,9 +5,9 @@ import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.router.*;
 import javax.servlet.http.HttpServletResponse;
 
-//@Route("404")
-public class CustomNotFoundView /*extends Div implements HasErrorParameter<NotFoundException> */{
-/*
+@Route("404")
+public class CustomNotFoundView extends Div implements HasErrorParameter<NotFoundException> {
+
     @Override
     public int setErrorParameter(BeforeEnterEvent event, ErrorParameter<NotFoundException> parameter) {
         H1 message = new H1("Die Seite konnte nicht gefunden werden (404)");
@@ -15,5 +15,4 @@ public class CustomNotFoundView /*extends Div implements HasErrorParameter<NotFo
         return HttpServletResponse.SC_NOT_FOUND;
     }
 
- */
 }
