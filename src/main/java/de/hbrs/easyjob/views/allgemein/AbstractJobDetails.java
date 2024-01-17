24de8a7b16eadcd2e11c6c9dd1598a302044f481
Janina Bovenkerk <janina.bovenkerk@smail.inf.h-brs.de>
2024-01-17 22:26:43 +0100

    Melden-Button zur JobDetailView hinzugefügt

--- src/main/java/de/hbrs/easyjob/views/allgemein/AbstractJobDetails.java
+++ src/main/java/de/hbrs/easyjob/views/allgemein/AbstractJobDetails.java
@@ -1,9 +1,15 @@
 package de.hbrs.easyjob.views.allgemein;
 
 import com.flowingcode.vaadin.addons.fontawesome.FontAwesome;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.contextmenu.ContextMenu;
+import com.vaadin.flow.component.contextmenu.MenuItem;
 import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.html.*;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.Scroller;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -11,9 +17,11 @@ import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.BeforeEvent;
 import com.vaadin.flow.router.HasUrlParameter;
+import de.hbrs.easyjob.controllers.MeldungController;
 import de.hbrs.easyjob.controllers.SessionController;
 import de.hbrs.easyjob.entities.Job;
 import de.hbrs.easyjob.entities.Unternehmensperson;
+import de.hbrs.easyjob.entities.Meldung;
 import de.hbrs.easyjob.repositories.JobRepository;
 
 @StyleSheet("JobDetails.css")
@@ -23,17 +31,21 @@ public abstract class AbstractJobDetails extends VerticalLayout implements Befor
 
     // Controllers
     protected final transient SessionController sessionController;
+    private final MeldungController meldungController;
 
     // Components
     public final VerticalLayout frame = new VerticalLayout();
     public final HorizontalLayout buttons = new HorizontalLayout();
     public final Div descriptionContainer = new Div();
 
+    private boolean isUnternehmensPerson;
+
     @Override
     public void beforeEnter(BeforeEnterEvent event) {
         if (!sessionController.isLoggedIn()) {
             event.rerouteTo(LoginView.class);
         }
+
     }
 
     @Override
@@ -62,9 +74,37 @@ public abstract class AbstractJobDetails extends VerticalLayout implements Befor
 
         // Zurück
         Button back = new Button("Zurück", FontAwesome.Solid.CHEVRON_LEFT.create());
+        back.addClickListener(e -> UI.getCurrent().getPage().getHistory().back());
         back.addClassName("job-details-back-button");
 
         buttons.add(back);
+
+        // Code für Melde-Funktion:
+        if (!isUnternehmensPerson) {
+
+            VerticalLayout dotsLayout = new VerticalLayout();
+            dotsLayout.setWidth("fit-content");
+
+            // Drei-Punkte-Icon für das Dropdown-Menü
+            Icon dots = new Icon(VaadinIcon.ELLIPSIS_DOTS_V);
+            dots.getStyle().set("cursor", "pointer");
+            dots.setSize("1em");
+
+            // Dropdown-Menü erstellen
+            ContextMenu contextMenu = new ContextMenu();
+            contextMenu.setTarget(dots);
+            contextMenu.setOpenOnClick(true);
+            MenuItem melden = contextMenu.addItem("Melden", e -> {
+                Meldung meldung = new Meldung();
+                meldungController.saveMeldung(meldung, job);
+                Notification.show("Gemeldet", 3000, Notification.Position.TOP_STRETCH);
+            });
+            melden.getElement().getStyle().set("color", "red");
+
+            dotsLayout.add(dots);
+            buttons.add(dotsLayout);
+        }
+
 
         // Unternehmen
         H3 company = new H3(job.getUnternehmen().getName());
@@ -109,10 +149,13 @@ public abstract class AbstractJobDetails extends VerticalLayout implements Befor
 
     protected AbstractJobDetails(
             SessionController sessionController,
-            JobRepository jobRepository
-    ) {
+            JobRepository jobRepository,
+            MeldungController meldungController) {
+
         this.jobRepository = jobRepository;
         this.sessionController = sessionController;
+        this.meldungController = meldungController;
+        isUnternehmensPerson = sessionController.hasRole("ROLE_UNTERNEHMENSPERSON");
 
         frame.setClassName("container");
         add(frame);
--- src/main/java/de/hbrs/easyjob/views/unternehmen/JobDetailsView.java
+++ src/main/java/de/hbrs/easyjob/views/unternehmen/JobDetailsView.java
@@ -5,6 +5,7 @@ import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.router.Route;
+import de.hbrs.easyjob.controllers.MeldungController;
 import de.hbrs.easyjob.controllers.SessionController;
 import de.hbrs.easyjob.entities.Job;
 import de.hbrs.easyjob.repositories.JobRepository;
@@ -14,8 +15,8 @@ import de.hbrs.easyjob.views.components.UnternehmenLayout;
 @Route(value = "unternehmen/job", layout = UnternehmenLayout.class)
 @StyleSheet("JobDetailsUnternehmen.css")
 public class JobDetailsView extends AbstractJobDetails {
-    public JobDetailsView(SessionController sessionController, JobRepository jobRepository) {
-        super(sessionController, jobRepository);
+    public JobDetailsView(SessionController sessionController, JobRepository jobRepository, MeldungController meldungController) {
+        super(sessionController, jobRepository, meldungController);
     }
 
     @Override
