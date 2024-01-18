556d549be71946787cf21952515be609497f6686
Sina Kosarifard <sina.kosarifard@smail.inf.h-brs.de>
2024-01-18 11:02:15 +0100

    CORS erlaubt

--- src/main/java/de/hbrs/easyjob/ApplicationNoSecurity.java
+++ src/main/java/de/hbrs/easyjob/ApplicationNoSecurity.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * Konfiguration für die Anwendung ohne Sicherheit
@@ -17,5 +19,18 @@ public class ApplicationNoSecurity {
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring()
                 .antMatchers("/**");
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // oder spezifische URL-Muster
+                        .allowedOrigins("http://sepp-test.inf.h-brs.de:8080")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
